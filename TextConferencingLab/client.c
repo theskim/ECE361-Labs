ac7e174a178a46eb10d98b4f67adf5a7d0014f90
mvernooy3687 <mattvernooy@gmail.com>
2023-11-27 14:40:42 -0500

    created registration

---
+++ TextConferencingLab/client.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h> 
+#include <errno.h>
+
+#define MAX_LINE 256
+#define MAX_NAME 20
+#define MAX_DATA 100
+
+int main(int argc, char * argv[]){
+
+    struct message {
+    unsigned int type;
+    unsigned int size;
+    unsigned char source[MAX_NAME];
+    unsigned char data[MAX_DATA];
+    };
+
+    //start by connecting/registering. user needs to pass host IP, host port. Then if successful, register by passing password and id
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char buf[MAX_LINE]; // buffer to store result from recvfrom
+    int deliver_socket;
+    struct stat statbuf; // used to store information about a file or directory
+    socklen_t addr_len = sizeof(sin); // ensure that it matches the size of the sin variable
+    char* str_end;
+
+    char command[MAX_LINE];
+    char ID[MAX_LINE];
+    char password[MAX_LINE];
+    char IP[MAX_LINE];
+    char port_passed[MAX_LINE];
+    if (scanf("%s %s %s %s %s", command, ID, password, IP, port_passed) != 5){
+        perror("should have five arguments");
+        close(deliver_socket);
+        exit(1);
+    }
+
+    hp = gethostbyname(IP);
+    if (!hp){
+        perror("unknown host");
+        exit(1);
+    }
+
+    bzero(&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+    
+    long port = strtol(port_passed, &str_end, 10);
+    if (errno == ERANGE || str_end == port_passed || *str_end != '\0'){
+        perror("invalid port");
+        exit(1);
+    }
+
+    sin.sin_port = htons(port); // Convert values between host and network byte order
+
+    if ((deliver_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    char register_request[MAX_LINE];
+    strcat(register_request,str2);
+    strcat(register_request,":");
+    strcat(register_request,str3);
+    
+
+    sendto(deliver_socket, register_request, strlen(register_request), 0, (struct sockaddr*) &sin, sizeof(sin));
+    if (recvfrom(deliver_socket, buf, sizeof(buf), 0, (struct sockaddr*) &sin, &addr_len) < 0){
+        perror("recvfrom");
+        close(deliver_socket);
+        exit(1);  
+    }
+
+    buf[strlen("registration_successful")] = '\0'; // safety
+    if (!strcmp(buf, "registration_successful")){ // buf == "registration_successful"
+        printf("Registered Successfully.\n"); //LO_ACK 
+    } else {
+        printf(buf); //LO_NAK
+        close(deliver_socket);
+        exit(0);
+    }
+
+}
---
+++ TextConferencingLab/server.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include "packet.h"
+
+#define MAX_NAME 20
+#define MAX_PASSWORD 30
+#define MAX_DATA 100
+
+//hard-coded users:
+char IDs[] = {"Steve","Jill","Grace","Joe"};
+char passwords[] = {"Steve1","Jill2","Grace3","Joe4"};
+
+struct client {
+    unsigned char IP[MAX_NAME];
+    unsigned int port;
+    unsigned int session_ID;
+    unsigned char ID[MAX_NAME];
+    struct client *next;
+    };
+
+struct message {
+    unsigned int type;
+    unsigned int size;
+    unsigned char source[MAX_NAME];
+    unsigned char data[MAX_DATA];
+    };
+
+
+struct client *head = NULL;
+
+client insert_client(unsigned char[] ID, unsigned char[] IP, unsigned int port)
+{
+    struct client *newNode = malloc(sizeof(struct client));
+    newNode->session_ID = -1;
+    newNode->ID = ID;
+    newNode->IP = IP;
+    newNode->port = port;
+    newNode->next = NULL;
+
+    struct client *traverser = head;
+    while(traverser->next!=NULL)
+        traverser = traverser->next;
+    
+    traverser->next = newNode;
+}
+
+int remove_client(unsigned char[] IP)
+{
+    if (head!=NULL)
+        if(head->next == NULL && !strcmp(head->IP,IP))
+            head = NULL;
+            return 1;
+        if(head->next == NULL && strcmp(head->IP,IP))
+            printf("ID not found");
+            return -1;
+    else
+        printf("List already empty");
+        return -1; 
+    
+    struct client *traverser = head->next;
+    struct client *prev_node = head;
+
+    while(traverser->next!=NULL && strcmp(traverser->IP,IP))
+        prev_node = prev_node->next;
+        traverser = traverser->next;
+
+    if(strcmp(traverser->IP,IP))
+        printf("ID not found");
+        return -1;
+
+    if(prev_node->next = traverser->next);
+}
+
+
+int main(int argc, char *argv[]){
+    struct sockaddr_in sin;
+    char buf[MAX_LINE * 3];
+    socklen_t addr_len = sizeof(sin);
+    int server_socket;
+    char* str_end;
+
+    //use me!
+    //https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/
+
+    if (argc != 2){
+        perror("Should have two arguments");
+        exit(1);
+    }
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        close(server_socket);
+        exit(1);
+    }
+
+    bzero(&sin, sizeof(sin)); // fills a buffer with zero bytes
+    sin.sin_family = AF_INET; // IPv4
+    sin.sin_addr.s_addr = INADDR_ANY; // update address (0.0.0.0)
+    long port = strtol(argv[1], &str_end, 10);
+
+    if (errno == ERANGE || str_end == argv[1] || *str_end != '\0'){ // error returned or nothing received
+        perror("invalid port");
+        close(server_socket);
+        exit(1);
+    }
+    sin.sin_port = htons(port); // Convert values between host and network byte order
+    
+    if ((bind(server_socket, (struct sockaddr*)&sin, sizeof(sin))) < 0) {
+        perror("bind");
+        close(server_socket);
+        exit(1);
+    }
+
+    if (recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr*) &sin, &addr_len) < 0){
+        perror("recvfrom");
+        close(server_socket);
+        exit(1);  
+    }
+
+    buf[strlen("ftp")] = '\0'; // safety
+    if (!strcmp(buf, "ftp")){
+        // reply with yes
+        sendto(server_socket, "yes", strlen("yes"), 0, (struct sockaddr*) &sin, sizeof(sin));
+    } else {
+        // reply with no
+        sendto(server_socket, "no", strlen("no"), 0, (struct sockaddr*) &sin, sizeof(sin));
+    }
+}
